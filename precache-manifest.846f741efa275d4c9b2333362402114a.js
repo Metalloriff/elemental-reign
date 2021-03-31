36365d9944434e52d3324a8c21248a688fcacc85
Metalloriff <37094912+Metalloriff@users.noreply.github.com>
2021-03-30 23:55:40 -0400

    First test run

---
+++ precache-manifest.846f741efa275d4c9b2333362402114a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c3188b31e289b27fedcbcb8d409b56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2dd6f66dd274340ef75c",
+    "url": "./static/css/main.df75945a.chunk.css"
+  },
+  {
+    "revision": "80e7ac4acbd262054855",
+    "url": "./static/js/2.26f7afb6.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "./static/js/2.26f7afb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd6f66dd274340ef75c",
+    "url": "./static/js/main.4d174cfc.chunk.js"
+  },
+  {
+    "revision": "2ae45ada7e7f3425f65a",
+    "url": "./static/js/runtime-main.82e1482a.js"
+  },
+  {
+    "revision": "6bb0b904b6d514682eefb322cd1e40b5",
+    "url": "./static/media/0.6bb0b904.png"
+  },
+  {
+    "revision": "a017289ed8be51454ee49ea89468159b",
+    "url": "./static/media/1.a017289e.png"
+  },
+  {
+    "revision": "52a18f12459189181079104e710d4092",
+    "url": "./static/media/10.52a18f12.png"
+  },
+  {
+    "revision": "004568d5ebaefa0450eb81079e8200ac",
+    "url": "./static/media/2.004568d5.png"
+  },
+  {
+    "revision": "faa32781564cf55b9dfcdc6aebf3b496",
+    "url": "./static/media/3.faa32781.png"
+  },
+  {
+    "revision": "e8ae3750783bc894bab5f1113e46bf61",
+    "url": "./static/media/4.e8ae3750.png"
+  },
+  {
+    "revision": "21b923042c79b2d4c278f3a7ca5baa3a",
+    "url": "./static/media/5.21b92304.png"
+  },
+  {
+    "revision": "24efbe96ca40441b05b29135c17b9584",
+    "url": "./static/media/6.24efbe96.png"
+  },
+  {
+    "revision": "526fc9fd4786a79ccc1f7f5428b1c433",
+    "url": "./static/media/7.526fc9fd.png"
+  },
+  {
+    "revision": "e87ae5958e4a5c23d4860bfca231fe78",
+    "url": "./static/media/8.e87ae595.png"
+  },
+  {
+    "revision": "ec1183d3d5e914545f9732362c31d420",
+    "url": "./static/media/9.ec1183d3.png"
+  },
+  {
+    "revision": "f3440e8ca4f45f2ebd22b10f59179785",
+    "url": "./static/media/Icon.f3440e8c.png"
+  }
+]);
